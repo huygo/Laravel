9fed9784d6d0b2340d15a0c4dbe9f3d337fce04b
huygo <quanghuy.pt97@gmail.com>
2020-04-23 22:37:35 +0700

    init project

---
+++ app/Http/Controllers/MyController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class MyController extends Controller
+{
+    public function xinchao(){
+        echo "Chào các bạn";
+    }
+    public function Hoten($ten){
+        echo "Tên là: ".$ten;
+        //gọi route
+        return redirect()->route('Test3');
+    }
+    public function Geturl(Request $request){
+         // return $request->path(); lay route goi ra thoi
+       // return $request->url(); lay toan bo url
+       //kt phuong thuc truyen get or post
+       if ($request->isMethod('get')) {
+           echo "Laf phuowng thuc get";
+       }else
+       echo "Khong la get";
+    }
+    public function postForm(Request $request){
+        echo $request->HoTen;
+    }
+    //cookie
+    public function setcookie(){
+        $response= new Response();
+        $response->withCookie('abc','Giá trị',1);
+        return $response;
+    }
+    public function getcookie(Request $request){
+        return $request->cookie('abc');
+    }
+    //upload file
+    public function postFile(Request $request){
+        //kiem tra file co ton tai khong
+        if ($request->hasFile('myFile')) {
+            $file=$request->file('myFile');
+            if ($file->getClientOriginalExtension('myFile') == "jpg") {//kiem tra dinh dang cia file
+                $filename=$file->getClientOriginalName('myFile');//lay ten file la ten file up len
+                $file->move('images',$filename);
+            }else{
+                echo "Khong dung dinh dang";
+            }
+        
+        }else{
+            echo "chua co file";
+        }
+    }
+    //Json
+    public function getJson(){
+        $array= ['Huy go','quanghuy','Laravel','html'];
+        return response()->json( $array);
+    }
+    //view
+    public function myView()
+    {
+        //return view('myView');
+        return view('layout.header');//goi view trong thu muc layout
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('Khoahoc' ,function(){
+    return "Chao xìn";
+});
+// truyen_tham_so
+Route::get('Hoten/{ten}', function ($ten) {
+    echo "Tên của bạn là: ".$ten;
+});
+//gán tên
+Route::get('Test1',[ 'as'=>'Test',function () {
+        echo "Gán test1 =test";
+}]);
+//gán tên 2
+Route::get('Test2', function () {
+    echo  "Đặt test2 có tên là test3";
+})->name('Test3');
+//gọi route
+Route::get('Goiten', function () {
+    return redirect()->route('Test3');
+});
+//Nhóm group
+Route::group(['prefix' => 'admin'], function () {
+     Route::get('users1', function () {
+         echo "User1";
+     });
+     Route::get('users2', function () {
+         echo "User2";
+     });
+     Route::get('users3', function () {
+         echo "User3";
+     });
+});
+//goi controller
+Route::get('Goicontroller','Mycontroller@xinchao' );
+//truyen du lieu cho controller
+Route::get('Truyendl/{ten}', 'Mycontroller@Hoten');
+//URL
+Route::get('Myrequest', 'Mycontroller@Geturl') ;
+
+//gui nhan dl voi request
+Route::get('Form', function () {
+    //goi view
+     return view('postForm');
+});
+
+Route::post('postForm', ['as'=>'postForm','uses'=>'Mycontroller@postForm']) ;
+//cookie
+Route::get('setcookie',  'Mycontroller@setcookie');
+Route::get('getcookie', 'Mycontroller@getcookie' );
+//upload file
+Route::get('uploadFile', function () {
+    return view('postFile');
+});
+Route::post('postFile', ['as'=>'postFile','uses'=>'MyController@postFile']);
+//Json
+Route::get('getJson','MyController@getJson' );
+//gọi view
+Route::get('goiview','MyController@myView' );
